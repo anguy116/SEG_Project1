95c8571c10af64e4dc6dab7f1ceea541ca49dc82
rashadrahman <rashadrahman98@gmail.com>
2018-10-25 22:12:40 -0400

    folder commit

--- Project1/app/src/main/java/com/example/andrew/project1/CreateProviderAccountActivity.java
+++ Project1/app/src/main/java/com/example/andrew/project1/CreateProviderAccountActivity.java
@@ -1,13 +1,80 @@
 package com.example.andrew.project1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
 
-public class CreateProviderAccountActivity extends AppCompatActivity {
-
+public class CreateProviderAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText editTextCompany, editTextPhone, editTextEmail, editTextPassword, editTextConfirmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_provider_account);
+        findViewById(R.id.saveInfo).setOnClickListener(this);
+        editTextCompany = (EditText)findViewById(R.id.companyName);
+        editTextPhone = (EditText)findViewById(R.id.phoneNumber);
+        editTextEmail = (EditText)findViewById(R.id.email);
+        editTextPassword = (EditText)findViewById(R.id.password);
+        editTextConfirmPassword = (EditText)findViewById(R.id.confirmPassword);
+    }
+    public void onClick(View view){
+        switch(view.getId()) {
+            case R.id.saveInfo:
+                if (registerUser()){
+                    startActivity(new Intent(this, LoggedInActivity.class));
+                }
+
+                break;
+        }
+    }
+    private boolean registerUser() {
+        boolean validCompany = true;
+        String company = editTextCompany.getText().toString().trim();
+        String phone = editTextPhone.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+
+        if (company.isEmpty()){
+            editTextCompany.setError("Company name is required");
+            editTextCompany.requestFocus();
+            validCompany = false;
+        }
+        if (phone.isEmpty()){
+            editTextPhone.setError("Phone number is required");
+            editTextPhone.requestFocus();
+            validCompany = false;
+        }
+        if (!phone.matches("^[0-9]+$")) {
+            editTextPhone.setError("Phone number must be valid");
+            editTextPhone.requestFocus();
+            validCompany = false;
+        }
+        if (email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            validCompany = false;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Email must be valid");
+            editTextEmail.requestFocus();
+            validCompany = false;
+        }
+        if (password.isEmpty()){
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            validCompany = false;
+        }
+        if (!password.equals(confirmPassword)){
+            editTextConfirmPassword.setError("Passwords must match");
+            editTextConfirmPassword.requestFocus();
+            validCompany = false;
+        }
+
+        return validCompany;
     }
 }
--- Project1/app/src/main/java/com/example/andrew/project1/LoginSignUp.java
+++ Project1/app/src/main/java/com/example/andrew/project1/LoginSignUp.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 
-public class LoginSignUp extends AppCompatActivity {
+public class LoginSignUp extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_sign_up);
+        findViewById(R.id.createAccountButton).setOnClickListener(this);
     }
 
     public void createAccountButton(View view) {
@@ -23,5 +24,13 @@ public class LoginSignUp extends AppCompatActivity {
         //Application Context and Activity
         Intent intent = new Intent(getApplicationContext(), LoggedInActivity.class);
         startActivityForResult (intent,0);
+    }
+
+    public void onClick(View view){
+        switch(view.getId()) {
+            case R.id.createAccountButton:
+                startActivity(new Intent(this, RoleActivity.class));
+                break;
+        }
     }
 }
